709ac196a3cdb2e0c6b83a11d2eebc58955520d5
nand2246 <nand.pats@gmail.com>
2021-04-05 22:25:01 -0400

    Updated navbar animation and fixed bugs

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+function rotateNavbarButton() {
+    let navButton = document.getElementById("navbar-button");
+    if (navButton.getAttribute("rotated") == "false") {
+        navButton.style.transform = "rotate(180deg)";
+        navButton.setAttribute("rotated", "true");
+    } else {
+        navButton.style.transform = "rotate(0deg)";
+        navButton.setAttribute("rotated", "false");
+    }
+}
