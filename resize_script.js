4c648b4d65578979d8cdf7dffe83d66d02a3082f
Nand <nand.pats@gmail.com>
2022-01-09 18:41:32 -0500

    Resized images in 2020 Winter collection

--- resize_script.js
+++ resize_script.js
@@ -1,9 +1,9 @@
 const sharp = require('sharp');
 const fs = require('fs');
-const directory = 'images/1-2020March';
+const directory = 'images/5-2020Winter';
 
 fs.readdirSync(directory).forEach(file => {
   sharp(`${directory}/${file}`)
-    .resize({width: 5000}) // width, height
+    .resize({width: 3000}) // width, height
     .toFile(`${file}`);
   });
